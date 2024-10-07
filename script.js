398b5bc64b0007b8677e6a621c8e0c1bb38dcc5b
shahnazj <shahnaz.jafari35@gmail.com>
2024-10-07 21:24:58 +0200

    jag har tränat lite datatyper via JavaScript

---
+++ script.js
@@ -0,0 +1,64 @@
+// en variabel = någonting som vi lagrar information i
+
+
+let firstName = "Shahnaz";
+const lastName = 'Jafri';
+const age = 37;
+
+
+//firstName= 25;
+// console.log(lastName);
+
+
+// console.log(firstName);
+// console.log(lastName);
+
+
+
+
+
+
+/*
+String
+Number
+BooLean
+NuLL
+Undefined
+Object
+Array
+*/
+
+
+
+
+// let text = 'Hej jag heter '+ firstName +' och jag är 37 år gammal'
+//text = "Hej jag heter Shanaz och jag är "+ age +" år gammal"
+
+
+//let text = `Hej jag heter ${firstName} och jag är ${ age} år gammal`
+
+
+//console.log(text)
+
+
+let num1 = 5;
+let num2 = '7';
+
+
+console.log(num1 + num2)
+
+
+let randomNr = Math.random() * 100
+
+
+// console = Math.round(randomNr);
+
+
+// console.log(randomNr);
+
+
+// num1 = num1 +1;
+// num1 += 4;
+// num1 ++;
+// num1 --;
+// console.log(num1)
